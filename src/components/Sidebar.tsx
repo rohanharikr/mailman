a89203aafd78fe9a858b28badd69156c7210ce89
thoran <rohanharikumar80@gmail.com>
2020-12-26 20:12:54 +0530

    Update Icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Sidebar = () => {
 	return(
-		<aside className="py-6 px-4 h-screen w-64 bg-gray-900 text-gray-300">
+		<aside className="flex flex-col py-6 px-4 h-screen w-64 bg-gray-900 text-gray-300">
 			<button className="w-full h-10 px-2 flex items-center text-left rounded-md text-sm bg-blue-600">
 				<div className="flex -space-x-1 overflow-hidden">
   					<img className="inline-block h-6 w-6 rounded-full bg-blue-700" src="https://nordicapis.com/wp-content/uploads/Profile-shot-recent.jpg" alt="avatar" />
@@ -12,12 +12,12 @@ const Sidebar = () => {
   					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M19 9l-7 7-7-7" />
 				</svg>
 			</button>
-			<ul className="mt-4 text-sm space-y-2">
+			<ul className="flex-1 mt-4 text-sm space-y-2">
 				<li>
 					<button className="py-2 px-2 w-full flex items-center rounded-md text-left bg-gray-800">
 						<span className="h-6 w-6 inline-flex items-center justify-center">
 							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5">
-		  						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+		  						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20" />
 							</svg>
 						</span>
 						<span className="ml-2">Inbox</span>
@@ -28,7 +28,7 @@ const Sidebar = () => {
 					<button className="py-2 px-2 w-full flex items-center rounded-md text-left hover:bg-gray-800">
 						<span className="h-6 w-6 inline-flex items-center justify-center">
 							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5">
-		  						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+		  						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20" />
 							</svg>
 						</span>
 						<span className="ml-2">Sent</span>
@@ -61,7 +61,7 @@ const Sidebar = () => {
 					<button className="py-2 px-2 w-full flex items-center rounded-md text-left hover:bg-gray-800">
 						<span className="h-6 w-6 inline-flex items-center justify-center">
 							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5">
-		  						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+		  						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
 							</svg>
 						</span>
 						<span className="ml-2">Archive</span>
@@ -69,6 +69,14 @@ const Sidebar = () => {
 					</button>
 				</li>
 			</ul>
+			<button className="py-2 px-2 w-full flex items-center rounded-md text-left bg-blue-600">
+				<span className="h-6 w-6 inline-flex items-center justify-center">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5">
+  						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+					</svg>
+				</span>
+				<span className="ml-2">Compose</span>
+			</button>
 		</aside>
 	);
 }
